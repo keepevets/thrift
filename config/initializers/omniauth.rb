7d5cd8728c324be7000afbe7a703dfc263be57b6
Stephen Peek <keepevets@gmail.com>
2013-02-09 23:38:22 -0500

    singly

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :singly, ENV['SINGLY_ID'], ENV['SINGLY_SECRET']
+  provider :singly, '7f82d2a13968262087793d2e470a30a5', 'e91a37f82d36c88ac1eebb56ac44eee9'
 end
